e249c93d6b808a85f86049718eecf725ffe90a77
Jihong Lee <jihong2jihong@gmail.com>
2022-10-11 10:31:42 +0900

    modify: rank_diff direction

--- modules/stats.py
+++ modules/stats.py
@@ -25,6 +25,8 @@ class Summary():
             defined_func = lambda x: x.pct_change(periods) * 100
         df_summary = self.df.groupby('name')[[c for c in self.df.columns if ('count' in c) or ('ratio' in c) or (c == 'rank') or ('rate' in c)]].transform(defined_func)
         df_summary.columns = [c.split('_count')[0] + f'_{summary_func}' for c in df_summary.columns]
+        if 'rank_diff' in df_summary.columns:
+            df_summary['rank_diff'] = -df_summary['rank_diff']
         if fillna:
             df_summary = df_summary.fillna(0)
         self.df_summary[summary_func][periods] = df_summary
