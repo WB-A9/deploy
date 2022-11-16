5f782e2b027378dc1d298f7ebc897a49b5e558d5
jihongleejihong <stellamea@yonsei.ac.kr>
2022-11-16 17:16:47 +0900

    update: adjust cache for sql

--- modules/db.py
+++ modules/db.py
@@ -3,14 +3,14 @@ from sqlalchemy import create_engine
 import pandas as pd
 import streamlit as st
 
-# @st.cache(allow_output_mutation=True)
+@st.experimental_singleton
 def _connect_db():
     db_connection_str = f"mysql+pymysql://{st.secrets['DB']['user']}:{st.secrets['DB']['pw']}@{st.secrets['DB']['host']}/{st.secrets['DB']['db_name']}"
     db_connection = create_engine(db_connection_str)
     conn = db_connection.connect()
     return db_connection
 
-# @st.cache()
+@st.experimental_memo(ttl=600)
 def load_data(db_names:str|list):
     db_connection = _connect_db()
     if isinstance(db_names, str):
@@ -20,15 +20,13 @@ def load_data(db_names:str|list):
         for db_name in db_names:
             df_list.append(pd.read_sql_table(table_name = db_name, con = db_connection))
         return df_list
-   
+
+@st.experimental_memo(ttl=600)   
 def get_by_query(query):
     db_connection = _connect_db()
     return pd.read_sql(sql= query, con = db_connection)
 
-def update_data(df, db_name):
-    db_connection = _connect_db()
-    df.to_sql(name= db_name, con=db_connection, if_exists='replace',index=False)
-
+@st.experimental_memo(ttl=600)
 def insert_data(df, db_name):
     db_connection = _connect_db()
     df.to_sql(name= db_name, con=db_connection, if_exists='append',index=False)
